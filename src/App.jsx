feca43f14db9283a61f3642de3582b9379ff8c85
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-02 12:49:01 +1300

    image transition issue fixed by reducing number of images to 5 from 9

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,8 @@ import cat2 from './images/cat2.jpg'
 import cat3 from './images/cat3.jpg'
 import cat4 from './images/cat4.jpg'
 import cat5 from './images/cat5.jpg'
-import cat6 from './images/cat6.jpg'
-import cat7 from './images/cat7.jpg'
-import cat8 from './images/cat8.jpg'
-import cat9 from './images/cat9.jpg'
 
-const images = [cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9]
+const images = [cat1, cat2, cat3, cat4, cat5]
 
 function App() {
   return (
