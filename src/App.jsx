cb0fc8b97a8f0ed61e81c62563ff305663a91ee4
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-02 12:52:42 +1300

    7 images seems to be the max without encountering poor animations so have set this as the number

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import cat2 from './images/cat2.jpg'
 import cat3 from './images/cat3.jpg'
 import cat4 from './images/cat4.jpg'
 import cat5 from './images/cat5.jpg'
+import cat6 from './images/cat6.jpg'
+import cat7 from './images/cat7.jpg'
 
-const images = [cat1, cat2, cat3, cat4, cat5]
+const images = [cat1, cat2, cat3, cat4, cat5, cat6, cat7]
 
 function App() {
   return (
