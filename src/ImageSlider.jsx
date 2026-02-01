9209376fe763c954256bcac0cff54b72a07f7387
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-02 11:33:11 +1300

    add onClick functions for carousel image slider

--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -5,13 +5,29 @@ import './App.css'
 function ImageSlider({ imageUrls }) {
   const [index, setIndex] = useState(0)
 
+  function nextImage() {
+    setIndex((index) => {
+      if (index === index.length) {
+        return imageUrls[0]
+      } else return index + 1
+    })
+  }
+
+  function prevImage() {
+    setIndex((index) => {
+      if (index === 0) {
+        return imageUrls.length - 1
+      } else return index - 1
+    })
+  }
+
   return (
     <div className="carousel">
       <img src={imageUrls[index]} className="img-slider" />
-      <button className="slider-btn slider-btn-left">
+      <button className="slider-btn slider-btn-left" onClick={prevImage}>
         <ArrowBigLeft />
       </button>
-      <button className="slider-btn slider-btn-right">
+      <button className="slider-btn slider-btn-right" onClick={nextImage}>
         <ArrowBigRight />
       </button>
     </div>
