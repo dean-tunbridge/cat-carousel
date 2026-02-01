69d10934d4af76a3d8624a871a1ceb3526e3f04a
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-02 11:08:32 +1300

    add aspect ration and image container class for styling

--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { ArrowBigLeft, ArrowBigRight } from 'lucide-react'
+import './App.css'
 
 function ImageSlider({ imageUrls }) {
   const [index, setIndex] = useState(0)
 
   return (
     <div className="carousel-img-slider">
-      <img src={imageUrls[index]} />
+      <img src={imageUrls[index]} className="img-slider" />
       <button>
         <ArrowBigLeft />
       </button>
