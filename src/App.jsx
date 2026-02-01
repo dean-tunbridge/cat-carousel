33ab12ccba8757f2d81cd5326db53e4b523c753e
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-02 10:35:07 +1300

    import all cat images to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import cat1 from './images/cat1.jpg'
+import cat2 from './images/cat2.jpg'
+import cat3 from './images/cat3.jpg'
+import cat4 from './images/cat4.jpg'
+import cat5 from './images/cat5.jpg'
+import cat6 from './images/cat6.jpg'
+import cat7 from './images/cat7.jpg'
+import cat8 from './images/cat8.jpg'
+import cat9 from './images/cat9.jpg'
+import cat10 from './images/cat10.jpg'
+
 import './App.css'
 
 function App() {
