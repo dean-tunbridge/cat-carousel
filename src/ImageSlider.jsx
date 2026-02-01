f08d38beba04ad9f267c3def5c8d83437474ff21
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-02-02 11:14:21 +1300

    add left and right button styling. set slider icon colour

--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -6,12 +6,12 @@ function ImageSlider({ imageUrls }) {
   const [index, setIndex] = useState(0)
 
   return (
-    <div className="carousel-img-slider">
+    <div className="carousel">
       <img src={imageUrls[index]} className="img-slider" />
-      <button>
+      <button className="slider-btn-left">
         <ArrowBigLeft />
       </button>
-      <button>
+      <button className="slider-btn-right">
         <ArrowBigRight />
       </button>
     </div>
